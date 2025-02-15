d7a250b23d380c571446ea51be4f8e4901343550
Pratima sah <Pratima88rk@gmail.com>
2025-02-15 21:00:49 +0545

    I added 2 to each iteration

--- For_loop.java
+++ For_loop.java
@@ -3,7 +3,7 @@ class Main {
     public static void main(String[] args) {
         int i;
         for(i=1;i<=5;i++){
-        System.out.println(i);
+        System.out.println(i+2);
         }
     }
 }

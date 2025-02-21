ce09ce3a338c53486a7378c9c39c2efdbb1f1c67
Pratima sah <pratima88rk@gmail.com>
2025-02-21 21:53:42 +0545

    Creating switch case example file

---
+++ Switch_case.java
@@ -0,0 +1,26 @@
+
+// Switch case
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int standard;
+        Scanner scano=new Scanner(System.in);
+        System.out.println("Enter the standard number");
+        standard=scano.nextInt();
+        
+        switch(standard){
+            case 1:
+                System.out.print("Kuldeep sah\b");
+                break;
+            case 3:
+                 System.out.print("Aayush sah\b");
+                 break;
+            case 10:
+                 System.out.print("Sima Sah and Mahadev Sah\b");
+                break;
+                
+        }
+         System.out.println("studies in "+standard);
+       
+    }
+}

e4c3641978641504663273bf3f2e56f59cd5175a
Pratima Sah <Pratima88rk@gmail.com>
2025-02-22 20:11:35 +0545

    I added for loop code

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("hellow java ");
+     int i;
+        for(i=1;i<=5;i++){
+        System.out.println(i+2);
+        }
+
     }
 }

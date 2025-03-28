ef317c652e4ef7cfff204abef772b4e0662e3ff8
Pratima sah <pratima88rk@gmail.com>
2025-03-28 22:06:45 +0545

    creating file of Sort() method application in array

---
+++ sort()_method.java
@@ -0,0 +1,18 @@
+// sorting array-sort()method
+
+//Note to use sort() method we need to import Arrays file from java.util package.
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        String[] names={"Pratima","Roshni","sima","Pooja"};
+       Arrays.sort(names);
+       System.out.println("sorted array is ;");
+//first method to print array
+       for(int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+       }
+//second method to print array using Arrays.toString() from Arrays file            
+            System.out.println(Arrays.toString(names));
+       
+    }
+}

8609ad1a37ee2fbdc7532a08dd16513ae5a9aeca
Pratima sah <pratima88rk@gmail.com>
2025-03-28 22:09:09 +0545

    Update sort()_method.java

--- sort()_method.java
+++ sort()_method.java
@@ -7,7 +7,7 @@ class Main {
         String[] names={"Pratima","Roshni","sima","Pooja"};
        Arrays.sort(names);
        System.out.println("sorted array is ;");
-//first method to print array
+//first method to print array Using for loop
        for(int i=0;i<names.length;i++){
             System.out.println(names[i]);
        }

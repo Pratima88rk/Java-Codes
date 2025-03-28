750752f72f6e3a5736887745dbf8946d238178af
Pratima sah <pratima88rk@gmail.com>
2025-03-28 21:54:29 +0545

    Creating String_array.java file

---
+++ String_array.java
@@ -0,0 +1,8 @@
+//String Array
+class Main {
+    public static void main(String[] args) {
+        String[] names={"Pratima","Roshni","sima","Pooja"};
+        System.out.println("biggest sister among four is "+names[3]);
+          System.out.println("Smallest sister among four is "+names[2]);
+    }
+}

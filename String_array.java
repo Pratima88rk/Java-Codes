dd5d61b0e4d38a20bca4f5a3d25aaa8f5303a122
Pratima Sah <Pratima88rk@gmail.com>
2025-03-29 19:33:23 +0545

    I added sisters name in array

--- String_array.java
+++ String_array.java
@@ -1,7 +1,7 @@
 //String Array
 class Main {
     public static void main(String[] args) {
-        String[] names={"Pratima","Roshni","sima","Pooja"};
+        String[] names={"Pratima","Roshni","sima","Pooja","Roselyene ", "Megha"};  // I added two sister names "Roselynene" and "Megha"
         System.out.println("biggest sister among four is "+names[3]);
           System.out.println("Smallest sister among four is "+names[2]);
     }
